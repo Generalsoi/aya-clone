e1468fdc7a92f9103ed228e14dc8bbf8e50ed916
Generalsoi <ikhinobelesuccess@gmail.com>
2021-01-11 20:50:53 +0100

    built the switch tabs for the various buy options

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -51,3 +51,25 @@ function move() {
         document.querySelector('.countdown').innerText = time;
     }
 }
+
+
+
+////////////////////////////////////////////
+///// Swap between tabs in form-content/////
+////////////////////////////////////////////
+
+let tabPanes = document.getElementsByClassName("tab-header")[0].getElementsByTagName("div");
+
+for (let i=0; i < tabPanes.length; i++){
+    tabPanes[i].addEventListener('click',  power = function(){
+        document.getElementsByClassName("tab-header")
+        [0].getElementsByClassName("active")[0].classList.remove('active');
+
+    tabPanes[i].classList.add("active");
+
+    document.getElementsByClassName("tab-content")
+    [0].getElementsByClassName("active")[0].classList.remove('active');
+    document.getElementsByClassName("tab-content")
+    [0].getElementsByClassName("tab-body")[i].classList.add('active');
+    });
+}
